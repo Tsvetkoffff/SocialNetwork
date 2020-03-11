343b156f3a89797457f6d04877cbc4d854d24494
tsvetkoffff <tsvetkoffff@gmail.com>
2020-03-11 17:34:52 +0300

    Created a starting structure

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './Main.module.css';
+
+const Main = () => {
+    return (
+        <main className={s.main}>
+            Main
+        </main>
+    )
+}
+
+export default Main
