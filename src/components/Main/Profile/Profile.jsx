526ade337ccd45d58b2d1954eae33f86ce319a9f
tsvetkoffff <tsvetkoffff@gmail.com>
2020-03-23 20:15:45 +0300

    Refactor state in Profile

--- src/components/Main/Profile/Profile.jsx
+++ src/components/Main/Profile/Profile.jsx
@@ -3,17 +3,11 @@ import s from './Profile.module.css';
 import Discription from "./Discription/Discription";
 import Posts from "./Posts/Posts";
 
-// let postsData = [
-//     { id: 1, message: 'Post 1', likesCount: 45 },
-//     { id: 2, message: 'Post 2', likesCount: 54 },
-//     { id: 3, message: 'Post 3', likesCount: 67 }
-// ];
-
 const Profile = (props) => {
     return (
         <section className={s.profile}>
             <Discription />
-            <Posts posts={props.posts}/>
+            <Posts state={props.state.postsData}/>
         </section>
     )
 };
