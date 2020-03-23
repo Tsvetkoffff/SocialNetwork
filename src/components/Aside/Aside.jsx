0465dd09bac0dc03369aa0b2a860611721e735b6
tsvetkoffff <tsvetkoffff@gmail.com>
2020-03-23 20:13:17 +0300

    refactor aside

--- src/components/Aside/Aside.jsx
+++ src/components/Aside/Aside.jsx
@@ -1,37 +1,13 @@
 import React from 'react';
 import s from './Aside.module.css';
-import { NavLink } from "react-router-dom";
+import Nav from './Nav/Nav';
+import Frends from './Frends/Frends';
 
-const Aside = () => {
+const Aside = (props) => {
     return (
         <aside className={s.aside}>
-            <ul className={s.nav_list}>
-                <li className={s.nav_item}>
-                    <NavLink to='/Profile' className={s.nav_link} activeClassName={s.active}>
-                        Profile
-                    </NavLink>
-                </li>
-                <li className={s.nav_item}>
-                    <NavLink to='/Messages' className={s.nav_link} activeClassName={s.active}>
-                        Messages
-                    </NavLink>
-                </li>
-                <li className={s.nav_item}>
-                    <NavLink to='/News' className={s.nav_link} activeClassName={s.active}>
-                        News
-                    </NavLink>
-                </li>
-                <li className={s.nav_item}>
-                    <NavLink to='/Music' className={s.nav_link} activeClassName={s.active}>
-                        Music
-                    </NavLink>
-                </li>
-                <li className={s.nav_item}>
-                    <NavLink to='/Settings' className={s.nav_link} activeClassName={s.active}>
-                        Settings
-                    </NavLink>
-                </li>
-            </ul>
+            <Nav />
+            <Frends state={props.state.frendsData} />
         </aside>
     )
 };
