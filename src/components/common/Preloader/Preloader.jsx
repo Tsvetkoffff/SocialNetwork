f8083d7d6bbd066e38c2fb468b6c94f017701076
Tsvetkoffff <tsvetkoffff@gmail.com>
2020-04-23 14:33:23 +0300

    Lesson 57 - preloader, add come css for beauty

--- src/components/common/Preloader/Preloader.jsx
+++ src/components/common/Preloader/Preloader.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import preloaderImg from "../../../assets/img/preloader-1s-200px.svg"
+import preloaderImg from "../../../assets/img/preloader-1s-200px.svg";
+import s from "./Preloader.module.css"
 
 let Preloader = (props) => {
-    return <img src={preloaderImg} alt=""/>
+    return <img src={preloaderImg} alt="" className={s.preloader}/>
 };
 
 export default Preloader
